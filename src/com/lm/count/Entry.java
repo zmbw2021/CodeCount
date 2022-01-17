7f5240653d7505feef7ce90856534aa16611ee0f
zmbw2021 <78288024+zmbw2021@users.noreply.github.com>
2022-01-17 17:54:43 +0800

    Update Entry.java

--- src/com/lm/count/Entry.java
+++ src/com/lm/count/Entry.java
@@ -1,40 +1,52 @@
 package com.lm.count;
 
+import com.lm.count.constants.CountConstants;
 import com.lm.count.counter.CommonCodeCounter;
 import com.lm.count.counter.Counter;
 import com.lm.count.exception.CountCodeException;
+import com.lm.count.model.ConfigModel;
 import com.lm.count.result.CountResult;
 import com.lm.count.result.FileResult;
 import com.lm.count.utils.FileUtils;
 
 import java.io.File;
 import java.util.List;
+import java.util.Objects;
 
 /**
- * 描述
+ * 代码量计算入口
  *
  * @author limin
- * @date 2021/12/30
+ * @since 2021-12-30
  */
 public class Entry {
-    public static void main(String[] args) {
+    private ConfigModel model;
+
+    public Entry(ConfigModel model) {
+        this.model = model;
+    }
+
+    public CountResult codeCountCalculate() {
+        String filePath = model.getCheckPath();
         try {
-            FileUtils.checkFilePath(args[0]);
+            // todo 需要修改异常处理方式
+            FileUtils.checkFilePath(filePath);
         } catch (CountCodeException e) {
             System.out.println("发生错误：" + e.getLocalizedMessage());
-            return;
+            return null;
         }
         long startTime = System.currentTimeMillis();
-        List<String> allSubFilePath = FileUtils.getAllSubFilePath(args[0]);
-        CountResult countResult = Entry.countFiles(allSubFilePath);
+        List<String> allSubFilePath = FileUtils.getAllSubFilePath(filePath);
+        CountResult countResult = countFiles(allSubFilePath);
         countResult.print();
         long endTime = System.currentTimeMillis();
         System.out.println("执行完成，用时：" + (endTime - startTime) + "ms");
+        return countResult;
     }
 
-    private static CountResult countFiles(List<String> filePath) {
+    private CountResult countFiles(List<String> filePath) {
         CountResult countResult = new CountResult();
-        filePath.stream().map(Entry::countFileCode).forEach(fileResult -> {
+        filePath.stream().map(this::countFileCode).filter(Objects::nonNull).forEach(fileResult -> {
             countResult.addFileResult(fileResult);
             countResult.setSumLine(fileResult.getSumLine() + countResult.getSumLine());
             countResult.setCodeLine(fileResult.getCodeLine() + countResult.getCodeLine());
@@ -44,10 +56,13 @@ public class Entry {
         return countResult;
     }
 
-    private static FileResult countFileCode(String filePath) {
+    private FileResult countFileCode(String filePath) {
+        String checkType = model.getCheckType();
+        String[] supportedFileSuffix = checkType.split(CountConstants.SEMICOLON);
         File file = new File(filePath);
-        if (!FileUtils.getFileSuffix(file).equals("java")) {
-            System.out.println("当前暂不支持非Java文件，文件名：" + filePath + "，忽略该文件");
+        if (!FileUtils.containsFileSuffix(supportedFileSuffix, FileUtils.getFileSuffix(file))) {
+            System.out.println("当前暂不支持不在此后缀列表(" + checkType + ")中的文件，文件名：" + filePath + "，忽略该文件");
+            return null;
         }
         Counter counter = new CommonCodeCounter();
         return counter.count(file);
